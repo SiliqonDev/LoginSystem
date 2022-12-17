ee254bfb2c3a9f2b03a86cbeb75626d7b69b7c7a
WonkyPigs <harshit9mittal@gmail.com>
2022-12-17 14:56:50 +0530

    update checker quick fix

--- src/main/java/dev/wonkypigs/loginsystem/LoginSystem.java
+++ src/main/java/dev/wonkypigs/loginsystem/LoginSystem.java
@@ -2,6 +2,7 @@ package dev.wonkypigs.loginsystem;
 
 import dev.wonkypigs.loginsystem.commands.*;
 import dev.wonkypigs.loginsystem.listeners.PlayerJoinLeaveListener;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,16 +12,37 @@ public final class LoginSystem extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("LoginSystem has been enabled.");
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        registerCommands();
+        registerListeners();
 
-        getServer().getPluginManager().registerEvents(new PlayerJoinLeaveListener(), this);
-        getCommand("login").setExecutor(new LoginCommand());
-        getCommand("setpassword").setExecutor(new SetpasswordCommand());
-        getCommand("changepassword").setExecutor(new ChangepasswordCommand());
+        int pluginId = 17094;
+        Metrics metrics = new Metrics(this, pluginId);
+
+        UpdateChecker updateChecker = new UpdateChecker();
+        updateChecker.check();
+
+        getLogger().info("LoginSystem has been enabled.");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("LoginSystem has been disabled.");
+    }
+
+    public void registerCommands() {
+        getCommand("login").setExecutor(new LoginCommand());
+        getCommand("setpassword").setExecutor(new SetpasswordCommand());
+        getCommand("changepassword").setExecutor(new ChangepasswordCommand());
+    }
+
+    public void registerListeners() {
+        getServer().getPluginManager().registerEvents(new PlayerJoinLeaveListener(), this);
+        getServer().getPluginManager().registerEvents(new UpdateChecker(), this);
+    }
+
+    public static LoginSystem getInstance() {
+        return getPlugin(LoginSystem.class);
     }
 }
